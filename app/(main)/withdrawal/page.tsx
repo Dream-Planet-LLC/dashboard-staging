71469b100b8ec297567b8723a69a9774f8c1d554
Dream Planet <help.dreamplanet@gmail.com>
2026-06-02 13:18:28 -0700

    www

--- app/(main)/withdrawal/page.tsx
+++ app/(main)/withdrawal/page.tsx
@@ -199,7 +199,7 @@ const WithdrawalPage = () => {
   const pageSize = 20;
   const totalWithdrawals = pagination?.totalDocs || 0;
 
-  // API functions
+  // API functio
   const fetchWithdrawals = async () => {
     try {
       if (isInitialLoad) {
